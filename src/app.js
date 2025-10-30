c527dd1b62ee9d128ef8f16125c419a9dcc1cb1e
Asif Alam <104073769+Looperrrrrr@users.noreply.github.com>
2025-10-30 16:22:43 +0000

    done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(generarSuitAleatorio());
+  document.querySelector(".card").innerHTML = (generarNumeroAleatorio());
 };
+
+
+let generarNumeroAleatorio = () => {
+
+  let numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let indexNumeros = Math.floor(Math.random() * numeros.length);
+  return numeros[indexNumeros];
+};
+
+
+let generarSuitAleatorio = () => {
+
+  let suit = ["spade", "heart", "club", "diamond"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
+
+
